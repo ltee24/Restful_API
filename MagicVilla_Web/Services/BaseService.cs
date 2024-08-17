781526eb92404d6ea6226d2f0f71056c5dc56581
Jokanola Abdullateef <ayomidejokanola08@gmail.com>
2024-08-17 10:06:28 +0100

    Consuming API

---
+++ MagicVilla_Web/Services/BaseService.cs
@@ -0,0 +1,21 @@
+﻿using MagicVilla_Web.Models;
+using MagicVilla_Web.Repository;
+using MagicVilla_Web.Services.IServices;
+
+namespace MagicVilla_Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public APIResponse responseModel { get;  set; }
+        public BaseService()
+        {
+            this.responseModel = new();
+                
+        }
+
+        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MagicVilla_Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,11 @@
+﻿using MagicVilla_Web.Models;
+using MagicVilla_Web.Repository;
+
+namespace MagicVilla_Web.Services.IServices
+{
+    public interface IBaseService
+    {
+        APIResponse responseModel { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
